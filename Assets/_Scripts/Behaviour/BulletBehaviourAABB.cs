99a30cc966f7f1f04162d517e43e82548e887289
Kiera Bacon <kierajbacon@outlook.com>
2020-12-19 20:23:41 -0500

    Added Repo

---
+++ Assets/_Scripts/Behaviour/BulletBehaviourAABB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviourAABB : MonoBehaviour
+{
+    [Header("Bullet Attributes")]
+    public float speed;
+    public Vector3 direction;
+    public float range;
+
+    public BulletManager bulletManager;
+    public Vector3 collisionNormal;
+    public CubeBehaviour cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletManager = FindObjectOfType<BulletManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+    private void _Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void _CheckBounds()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) > range)
+        {
+            bulletManager.ReturnBullet(this.gameObject);
+        }
+    }
+}
